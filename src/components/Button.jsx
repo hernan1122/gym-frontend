34b27faaf395d8cf17dba88dc00211405b5e4939
Axel Torrico <torricohernan2@gmail.com>
2023-09-22 23:51:39 -0300

    se creo el componente Phrases

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Button({children}) {
   return (
-    <button className='w-36 h-12 text-black font-bold rounded-lg bg-yellow-400 transition-all duration-300'>
+    <button className='w-36 h-12 text-black font-bold rounded-lg bg-yellow-500 transition-all duration-300'>
       {children}
     </button>
   );
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -3,12 +3,13 @@ import InitialButtons from '../components/InitialButtons';
 
 // icons
 import logoApp1 from '../images/estacion-de-gimnasio.png'
+import Phrases from '../components/Phrases';
 
 function Home() {
   return (
     <div className='w-full h-screen flex flex-col items-center'>
       <div className='w-40 h-40 absolute top-14 rounded-lg transform rotate-45 bg-yellow-500'></div>
-      <div className='w-full mt-28 mb-32 relative flex justify-center items-center'>
+      <div className='w-full mt-28 mb-52 relative flex justify-center items-center'>
         <h1 className='text-4xl text-white font-semibold z-10'>AppGym</h1>
         <img className='w-28 absolute' src={logoApp1} alt="Logo de la app" />
       </div>
