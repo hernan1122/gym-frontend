ba8a8405ab15f4a52af9f6b96b831be72a8fb189
Axel Torrico <torricohernan2@gmail.com>
2023-09-22 23:51:21 -0300

    se creo el componente Phrases

---
+++ src/components/Phrases.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Phrases() {
+  return (
+    <div className='mt-10 p-10 bg-yellow-500 opacity-100'>
+      <p className='text-white'>Hola Axel, sé que esta vez lo lograrás.</p>
+    </div>
+  );
+}
+
+export default Phrases;
