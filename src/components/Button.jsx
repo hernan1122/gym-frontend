74474ad8fa3cc1255f561b9feb926fb2355e1c6a
Axel Torrico <torricohernan2@gmail.com>
2023-09-23 00:06:21 -0300

    se modificaron estilos

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Button({children}) {
   return (
-    <button className='w-56 h-12 text-lg text-black font-bold rounded-lg bg-yellow-500 transition-all duration-300'>
+    <button className='w-64 h-12 text-lg text-black font-bold rounded-lg bg-yellow-500 transition-all duration-300'>
       {children}
     </button>
   );
