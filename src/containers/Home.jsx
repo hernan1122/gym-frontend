ba94169d96139b4b020f2349cfe8c26bf6e5b4c2
Axel Torrico <torricohernan2@gmail.com>
2023-09-23 00:11:10 -0300

    se modificaron estilos

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -9,6 +9,7 @@ import Phrases from '../components/Phrases';
 function Home() {
   return (
     <div className='w-full h-screen flex flex-col items-center'>
+      <div className='w-full h-32 absolute top-28 bg-yellow-600'></div>
       <div className='w-40 h-40 absolute top-24 rounded-lg transform rotate-45 bg-yellow-500'></div>
       <div className='home-title w-full mb-40 flex justify-center items-center'>
         <h1 className='text-4xl text-white font-semibold z-10'>AppGym</h1>
