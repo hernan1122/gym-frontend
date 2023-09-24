3ffb24230e9ed529e777d6c79b39dc11b2fae4d5
Axel Torrico <torricohernan2@gmail.com>
2023-09-23 22:37:19 -0300

    se crearon las rutas para los diferentes botones

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../containers/Home';
+import Back from '../containers/Back';
+import Chest from '../containers/Chest';
+import Biceps from '../containers/Biceps';
+import Triceps from '../containers/Triceps';
+import Shoulders from '../containers/Shoulders';
+import Legs from '../containers/Legs';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/back' element={<Back />} />
+        <Route path='/chest' element={<Chest />} />
+        <Route path='/biceps' element={<Biceps />} />
+        <Route path='/triceps' element={<Triceps />} />
+        <Route path='/shoulders' element={<Shoulders />} />
+        <Route path='/legs' element={<Legs />} />
       </Routes>
     </BrowserRouter>
   );
