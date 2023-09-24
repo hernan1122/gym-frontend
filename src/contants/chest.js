cdfe83eba97dcf29ced599a41c1bf498364f0fa1
Axel Torrico <torricohernan2@gmail.com>
2023-09-24 11:23:25 -0300

    se creo el objeto chest

---
+++ src/contants/chest.js
@@ -0,0 +1,75 @@
+import aperturasDeclinadas from '../ejercicios/ejercicios-de-pecho/aperturas-declinadas.jpg'
+import aperturasInclinadas from '../ejercicios/ejercicios-de-pecho/aperturas-inclinadas.jpg'
+import aperturasPlanas from '../ejercicios/ejercicios-de-pecho/aperturas-planas.jpg'
+import cruceConPoleaAlto from '../ejercicios/ejercicios-de-pecho/cruce-con-polea-alto.jpg'
+import cruceConPoleaBajo from '../ejercicios/ejercicios-de-pecho/cruce-con-polea-bajo.jpg'
+import cruceConPolea from '../ejercicios/ejercicios-de-pecho/cruce-con-polea.jpg'
+import mariposa from '../ejercicios/ejercicios-de-pecho/mariposa.jpg'
+import pressBancoDeclinado from '../ejercicios/ejercicios-de-pecho/press-banco-declinado.jpg'
+import pressBancoInclinado from '../ejercicios/ejercicios-de-pecho/press-banco-inclinado.jpg'
+import pressBancoPlano from '../ejercicios/ejercicios-de-pecho/press-banco-plano.jpg'
+import pressConMancuerna from '../ejercicios/ejercicios-de-pecho/press-con-mancuerna.jpg'
+import pressConMancuernasDeclinadas from '../ejercicios/ejercicios-de-pecho/press-con-mancuernas-declinadas.jpg'
+import pressConMancuernasInclinadas from '../ejercicios/ejercicios-de-pecho/press-con-mancuernas-inclinadas.jpg'
+import pullover from '../ejercicios/ejercicios-de-pecho/pullover.jpg'
+
+const data_chest = [
+  {
+    name: 'Aperturas declinadas',
+    image: aperturasDeclinadas
+  },
+  {
+    name: 'Aperturas inclinadas',
+    image: aperturasInclinadas
+  },
+  {
+    name: 'Aperturas planas',
+    image: aperturasPlanas
+  },
+  {
+    name: 'Cruce con polea alto',
+    image: cruceConPoleaAlto
+  },
+  {
+    name: 'Cruce con polea bajo',
+    image: cruceConPoleaBajo
+  },
+  {
+    name: 'Cruce con polea',
+    image: cruceConPolea
+  },
+  {
+    name: 'Mariposa',
+    image: mariposa
+  },
+  {
+    name: 'Press banco declinado',
+    image: pressBancoDeclinado
+  },
+  {
+    name: 'Press banco inclinado',
+    image: pressBancoInclinado
+  },
+  {
+    name: 'press banco plano',
+    image: pressBancoPlano
+  },
+  {
+    name: 'Press con mancuerna',
+    image: pressConMancuerna
+  },
+  {
+    name: 'Press con mancuernas declinadas',
+    image: pressConMancuernasDeclinadas
+  },
+  {
+    name: 'Press con mancuernas inclinadas',
+    image: pressConMancuernasInclinadas
+  },
+  {
+    name: 'Pullover',
+    image: pullover
+  }
+]
+
+export default data_chest;
