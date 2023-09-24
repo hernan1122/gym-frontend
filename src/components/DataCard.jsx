be42955ecfe4b8d27c416822c1dc7134a7648aad
Axel Torrico <torricohernan2@gmail.com>
2023-09-24 13:27:17 -0300

    al apretar la imagen se agranda

--- src/components/DataCard.jsx
+++ src/components/DataCard.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function DataCard({name, image}) {
+function DataCard({ name, image }) {
+  const [isImageExpanded, setIsImageExpanded] = useState(false)
+
+  const toggleImageSize = () => {
+    setIsImageExpanded(!isImageExpanded)
+  }
+
+  const imageSizeClassName = isImageExpanded ? 'w-screen' : 'w-full rounded-lg'
+
   return (
-    <div className='border-color p-5 pt-3 border-b'>
+    <div
+      className='border-color p-5 pt-3 border-b'
+      onClick={toggleImageSize}
+    >
       <h2 className='mb-2 text-white text-lg'>{name}</h2>
-      <img className='w-full rounded-lg' src={image} alt="Imagen de ejercicio" />
+      <div className={`${isImageExpanded ? 'w-full h-screen fixed top-0 left-0 flex items-center justify-center bg-black z-50' : ''}`}>
+        <img className={imageSizeClassName} src={image} alt="Imagen de ejercicio" />
+      </div>
     </div>
   );
 }
