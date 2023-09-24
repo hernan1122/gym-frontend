9508c7982200cd840cfb827b97531234f370c056
Axel Torrico <torricohernan2@gmail.com>
2023-09-24 11:23:45 -0300

    se creo el objeto legs

---
+++ src/contants/legs.js
@@ -0,0 +1,45 @@
+import camillaCuadriceps from '../ejercicios/ejercicios-de-piernas/camilla-cuadriceps.jpg'
+import camillaPosterior from '../ejercicios/ejercicios-de-piernas/camilla-posterior.jpg'
+import estocada from '../ejercicios/ejercicios-de-piernas/estocada.jpg'
+import gemelosParadosPrensa from '../ejercicios/ejercicios-de-piernas/gemelos-parados-y-prensa.jpg'
+import mediaSentadilla from '../ejercicios/ejercicios-de-piernas/media-sentadilla.jpg'
+import pesoMuerto from '../ejercicios/ejercicios-de-piernas/peso-muerto.jpg'
+import prensa from '../ejercicios/ejercicios-de-piernas/prensa.jpg'
+import sentadila from '../ejercicios/ejercicios-de-piernas/sentadila.jpg'
+
+const data_legs = [
+  {
+    name: 'Camilla cuadriceps',
+    image: camillaCuadriceps
+  },
+  {
+    name: 'Camilla posterior',
+    image: camillaPosterior
+  },
+  {
+    name: 'Estocada',
+    image: estocada
+  },
+  {
+    name: 'Gemelos parados prensa',
+    image: gemelosParadosPrensa
+  },
+  {
+    name: 'Media sentadilla',
+    image: mediaSentadilla
+  },
+  {
+    name: 'Peso muerto',
+    image: pesoMuerto
+  },
+  {
+    name: 'Prensa',
+    image: prensa
+  },
+  {
+    name: 'Sentadila',
+    image: sentadila
+  }
+]
+
+export default data_legs;
