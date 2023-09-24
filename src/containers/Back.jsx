2d9d8259342fee25d8f0397872de33b73fad396e
Axel Torrico <torricohernan2@gmail.com>
2023-09-24 11:28:18 -0300

    se modificaron estilos

--- src/containers/Back.jsx
+++ src/containers/Back.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import data_back from '../contants/back.js'
+import data_back from '../contants/back'
 import HeaderItems from '../components/HeaderItems.jsx';
 import DataCard from '../components/DataCard.jsx';
 
 function Back() {
-
   return (
     <div className='pt-5'>
       <HeaderItems
         nameBorder='Es'
         nameSolid='palda'
       />
-      {data_back.map((back, index) => {
-        return (
-          <DataCard 
-            key={index}
-            name={back.name}
-            image={back.image}
-          />
-        )
-      })}
+      <div className='mt-14'>
+        {data_back.map((back, index) => {
+          return (
+            <DataCard 
+              key={index}
+              name={back.name}
+              image={back.image}
+            />
+          )
+        })}
+      </div>
     </div>
   );
 }
