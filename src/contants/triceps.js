3587f899da55b47e102162d9103ab0b729273299
Axel Torrico <torricohernan2@gmail.com>
2023-09-24 11:25:08 -0300

    se creo el objeto triceps

---
+++ src/contants/triceps.js
@@ -0,0 +1,60 @@
+import barraRomana from '../ejercicios/ejercicios-de-triceps/barra-romana.jpg'
+import barraWFrente from '../ejercicios/ejercicios-de-triceps/barra-w-frente.jpg'
+import frances2 from '../ejercicios/ejercicios-de-triceps/frances-2.jpg'
+import jalonConEstribos from '../ejercicios/ejercicios-de-triceps/jalon-con-estribos.jpg'
+import patadaDeBurro from '../ejercicios/ejercicios-de-triceps/patada-de-burro.jpg'
+import tricepsAgarreV from '../ejercicios/ejercicios-de-triceps/triceps-agarre-v.jpg'
+import tricepsConMancuernaAtras from '../ejercicios/ejercicios-de-triceps/triceps-con-mancuerna-atras.jpg'
+import tricepsConPolea from '../ejercicios/ejercicios-de-triceps/triceps-con-polea.jpg'
+import tricepsConSogaAlta from '../ejercicios/ejercicios-de-triceps/triceps-con-soga-alta.jpg'
+import tricepsConSoga from '../ejercicios/ejercicios-de-triceps/triceps-con-soga.jpg'
+import tricepsFuerza from '../ejercicios/ejercicios-de-triceps/triceps-fuerza.jpg'
+
+const data_triceps = [
+  {
+    name: 'Barra romana',
+    image: barraRomana
+  },
+  {
+    name: 'Barra W frente',
+    image: barraWFrente
+  },
+  {
+    name: 'Frances 2',
+    image: frances2
+  },
+  {
+    name: 'Jalon con estribos',
+    image: jalonConEstribos
+  },
+  {
+    name: 'Patada de burro',
+    image: patadaDeBurro
+  },
+  {
+    name: 'Triceps agarre V',
+    image: tricepsAgarreV
+  },
+  {
+    name: 'Triceps con mancuerna atras',
+    image: tricepsConMancuernaAtras
+  },
+  {
+    name: 'Triceps con polea',
+    image: tricepsConPolea
+  },
+  {
+    name: 'Triceps con soga alta',
+    image: tricepsConSogaAlta
+  },
+  {
+    name: 'Triceps con soga',
+    image: tricepsConSoga
+  },
+  {
+    name: 'Triceps fuerza',
+    image: tricepsFuerza
+  }
+]
+
+export default data_triceps;
