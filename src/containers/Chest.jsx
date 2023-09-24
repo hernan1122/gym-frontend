0605dd4f0a8433b3e3c6aea835b5ad92f71e18b8
Axel Torrico <torricohernan2@gmail.com>
2023-09-24 11:31:11 -0300

    se llamo a data_chest y se mapeo

--- src/containers/Chest.jsx
+++ src/containers/Chest.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import data_chest from '../contants/chest'
 import HeaderItems from '../components/HeaderItems.jsx';
+import DataCard from '../components/DataCard.jsx';
 
 function Chest() {
   return (
@@ -8,6 +10,17 @@ function Chest() {
         nameBorder='Pe'
         nameSolid='cho'
       />
+      <div className='mt-14'>
+        {data_chest.map((chest, index) => {
+          return (
+            <DataCard 
+              key={index}
+              name={chest.name}
+              image={chest.image}
+            />
+          )
+        })}
+      </div>
     </div>
   );
 }
