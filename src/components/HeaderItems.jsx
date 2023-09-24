fd76b5c806026968ad8c0ed194407473a6b1f145
Axel Torrico <torricohernan2@gmail.com>
2023-09-24 11:27:40 -0300

    se modificaron estilos

--- src/components/HeaderItems.jsx
+++ src/components/HeaderItems.jsx
@@ -8,7 +8,7 @@ function HeaderItems({nameBorder, nameSolid}) {
   }
 
   return (
-    <div className='w-full h-14 px-5 flex justify-between items-center bg-yellow-500'>
+    <div className='w-full h-14 px-5 fixed flex justify-between items-center bg-yellow-500'>
       <div className='flex'>
         <h3 style={textStyle} className='text-2xl text-back font-semibold'>{nameBorder}</h3>
         <h3 className='text-2xl text-back font-semibold'>{nameSolid}</h3>
