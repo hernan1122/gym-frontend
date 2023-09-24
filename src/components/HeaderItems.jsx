634276999c4dd65ad21d40a837b007f674be2163
Axel Torrico <torricohernan2@gmail.com>
2023-09-24 00:30:11 -0300

    se creo el componente HeaderItems

---
+++ src/components/HeaderItems.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ButtonBack from './ButtonBack';
+
+function HeaderItems({nameBorder, nameSolid}) {
+  const textStyle = {
+    WebkitTextStroke: '1px black',
+    color: 'transparent'
+  }
+
+  return (
+    <div className='w-full h-14 px-5 flex justify-between items-center bg-yellow-600'>
+      <div className='flex'>
+        <h3 style={textStyle} className='text-2xl text-back font-semibold'>{nameBorder}</h3>
+        <h3 className='text-2xl text-back font-semibold'>{nameSolid}</h3>
+      </div>
+      <ButtonBack />
+    </div>
+  );
+}
+
+export default HeaderItems;
