c5428f5c2f2fe23da857c6768126039f119347a5
Axel Torrico <torricohernan2@gmail.com>
2023-09-24 00:56:42 -0300

    se modifico el icono de la app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../index.css'
 
 // icons
-import logoApp1 from '../images/estacion-de-gimnasio.png'
+import logoApp from '../images/atleta.png'
 
 function Header() {
   const textStyle = {
@@ -12,9 +12,9 @@ function Header() {
 
   return (
     <>
-      <div className='w-full h-32 absolute top-28 bg-yellow-600'></div>
-      <div className='w-40 h-40 absolute top-24 left-12 rounded-lg transform rotate-45 bg-yellow-500'></div>
-      <img className='logo-app w-28 absolute' src={logoApp1} alt="Logo de la app" />
+      <div className='w-full h-32 absolute top-28 bg-yellow-500'></div>
+      <div className='w-40 h-40 absolute top-24 left-12 rounded-lg transform rotate-45 bg-yellow-600'></div>
+      <img className='logo-app w-28 absolute' src={logoApp} alt="Logo de la app" />
       <div className='title-app mb-32 ml-40 flex flex-col items-end z-10'>
         <h2 className='text-2xl text-black font-bold'>Seamos</h2>
         <h2 style={textStyle} className='text-4xl  text-white font-semibold'>FITNESS</h2>
