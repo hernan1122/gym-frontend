1d9049cf3e1618272ef93044acbf6f3137912c46
Axel Torrico <torricohernan2@gmail.com>
2023-09-23 22:34:48 -0300

    se creo el archivo back simulando un objeto json

---
+++ src/contants/back.js
@@ -0,0 +1,65 @@
+import dominadas from '../ejercicios/ejercicios-de-espalda/dominadas.jpg'
+import dorsalPorAtras from '../ejercicios/ejercicios-de-espalda/dorsal-por-atras.jpg'
+import dorsalPorDelante from '../ejercicios/ejercicios-de-espalda/dorsal-por-delante.jpg'
+import dorsalConEstribo from '../ejercicios/ejercicios-de-espalda/dorsal-con-estribo.jpg'
+import remoConBarra from '../ejercicios/ejercicios-de-espalda/remo-con-barra.jpg'
+import remoFijoTProna from '../ejercicios/ejercicios-de-espalda/remo-fijo-t-prona.jpg'
+import remoFijoTSuperior from '../ejercicios/ejercicios-de-espalda/remo-fijo-t-superior.jpg'
+import remoInvertido from '../ejercicios/ejercicios-de-espalda/remo-invertido.jpg'
+import serruchoMancuerna from '../ejercicios/ejercicios-de-espalda/serrucho-mancuerna.jpg'
+import serruchoPolea from '../ejercicios/ejercicios-de-espalda/serrucho-polea.jpg'
+import espaldaBaja1 from '../ejercicios/ejercicios-de-espalda/espalda-baja-1.jpg'
+import espaldaBaja2 from '../ejercicios/ejercicios-de-espalda/espalda-baja-2.jpg'
+
+const data_back = [
+  {
+    name: 'Dominadas',
+    image: dominadas
+  },
+  {
+    name: 'Dorsal por atrás',
+    image: dorsalPorAtras
+  },
+  {
+    name: 'Dorsal por delante',
+    image: dorsalPorDelante
+  },
+  {
+    name: 'Dorsal con estribo',
+    image: dorsalConEstribo
+  },
+  {
+    name: 'Remo con barra',
+    image: remoConBarra
+  },
+  {
+    name: 'Remo fijo T prona',
+    image: remoFijoTProna
+  },
+  {
+    name: 'Remo fijo T superior',
+    image: remoFijoTSuperior
+  },
+  {
+    name: 'Remo invertido',
+    image: remoInvertido
+  },
+  {
+    name: 'Serrucho con mancuerna',
+    image: serruchoMancuerna
+  },
+  {
+    name: 'Serrucho con polea',
+    image: serruchoPolea
+  },
+  {
+    name: 'Espalda baja',
+    image: espaldaBaja1
+  },
+  {
+    name: 'Espalda baja',
+    image: espaldaBaja2
+  }
+]
+
+export default data_back;
