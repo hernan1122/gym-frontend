c6db905270009f636fd2c88dc629cbb70448f587
Axel Torrico <torricohernan2@gmail.com>
2023-09-24 11:24:25 -0300

    se creo el objeto shoulders

---
+++ src/contants/shoulders.js
@@ -0,0 +1,45 @@
+import pressArnold from '../ejercicios/ejercicios-de-hombros/press-arnold.jpg'
+import pressHombroMancuernas from '../ejercicios/ejercicios-de-hombros/press-hombro-mancuernas.jpg'
+import pressMilitarAdela from '../ejercicios/ejercicios-de-hombros/press-militar-adela.jpg'
+import remoErguidoEncogido from '../ejercicios/ejercicios-de-hombros/remo-erguido-encogido.jpg'
+import voladasFrontalesConDisco from '../ejercicios/ejercicios-de-hombros/voladas-frontales-con-disco.jpg'
+import voladasFrontalesConMancuernas from '../ejercicios/ejercicios-de-hombros/voladas-frontales-con-mancuernas.jpg'
+import voladasLaterales from '../ejercicios/ejercicios-de-hombros/voladas-laterales.jpg'
+import voladasPosteriores from '../ejercicios/ejercicios-de-hombros/voladas-posteriores.jpg'
+
+const data_shoulders = [
+  {
+    name: 'Press arnold',
+    image: pressArnold
+  },
+  {
+    name: 'Press hombro mancuernas',
+    image: pressHombroMancuernas
+  },
+  {
+    name: 'Press militar adela',
+    image: pressMilitarAdela
+  },
+  {
+    name: 'Remo erguido encogido',
+    image: remoErguidoEncogido
+  },
+  {
+    name: 'Voladas frontales con disco',
+    image: voladasFrontalesConDisco
+  },
+  {
+    name: 'Voladas frontales con mancuernas',
+    image: voladasFrontalesConMancuernas
+  },
+  {
+    name: 'Voladas laterales',
+    image: voladasLaterales
+  },
+  {
+    name: 'Voladas posteriores',
+    image: voladasPosteriores
+  }
+]
+
+export default data_shoulders;
