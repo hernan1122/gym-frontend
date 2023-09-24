6ab0e58ccc3e626ba0e670313c2e6ec8a0320c29
Axel Torrico <torricohernan2@gmail.com>
2023-09-24 11:23:02 -0300

    se creo el objeto biceps

---
+++ src/contants/biceps.js
@@ -0,0 +1,60 @@
+import alternado from '../ejercicios/ejercicios-de-biceps/alternado.jpg'
+import bancoScottBarraW from '../ejercicios/ejercicios-de-biceps/banco-scott-barra-w.jpg'
+import bancoScottMancuerna from '../ejercicios/ejercicios-de-biceps/banco-scott-mancuerna.jpg'
+import biceps21 from '../ejercicios/ejercicios-de-biceps/biceps-21.jpg'
+import bicepsMartillo from '../ejercicios/ejercicios-de-biceps/biceps-martillo.jpg'
+import bicepsMartilloCruzado from '../ejercicios/ejercicios-de-biceps/biceps-martillo-cruzado.jpg'
+import bicepsPoleaAlta from '../ejercicios/ejercicios-de-biceps/biceps-polea-alta.jpg'
+import bicepsPoleaBaja from '../ejercicios/ejercicios-de-biceps/biceps-polea-baja.jpg'
+import bicepsPredicador from '../ejercicios/ejercicios-de-biceps/biceps-predicador.jpg'
+import concentrado1 from '../ejercicios/ejercicios-de-biceps/concentrado-1.jpg'
+import concentrado2 from '../ejercicios/ejercicios-de-biceps/concentrado-2.jpg'
+
+const data_biceps = [
+  {
+    name: 'Alternado',
+    image: alternado
+  },
+  {
+    name: 'Banco scott barra W',
+    image: bancoScottBarraW
+  },
+  {
+    name: 'Banco scott con mancuerna',
+    image: bancoScottMancuerna
+  },
+  {
+    name: 'Biceps 21',
+    image: biceps21
+  },
+  {
+    name: 'Remo con barra',
+    image: bicepsMartillo
+  },
+  {
+    name: 'Biceps martillo cruzado',
+    image: bicepsMartilloCruzado
+  },
+  {
+    name: 'Biceps polea alta',
+    image: bicepsPoleaAlta
+  },
+  {
+    name: 'Biceps polea baja',
+    image: bicepsPoleaBaja
+  },
+  {
+    name: 'Biceps predicador',
+    image: bicepsPredicador
+  },
+  {
+    name: 'Concentrado 1',
+    image: concentrado1
+  },
+  {
+    name: 'Concentrado 2',
+    image: concentrado2
+  }
+]
+
+export default data_biceps;
